16142879f062dd2afb553d503730b43896d5f904
Kenneth Phang <bunnyppl@gmail.com>
2018-04-24 19:17:17 +0800

    fix: remove about page

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express"),
      bodyParser = require("body-parser"),
-     cors = require('cors'),
+     cors = require('cors');
      
 var app = express();
 
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(express.static(__dirname + "../../dist"));
+
+const NODE_PORT = process.env.PORT;
 
 app.listen(NODE_PORT, function () {
     console.info("App server started on port " + NODE_PORT);
