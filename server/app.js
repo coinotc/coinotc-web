d24df297e4104ca22a6edc8ee6e3cb590f99a0da
Kenneth Phang <bunnyppl@gmail.com>
2018-04-24 19:06:45 +0800

    feat: added server side

---
+++ server/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require("express"),
+     bodyParser = require("body-parser"),
+     cors = require('cors'),
+     
+var app = express();
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.use(express.static(__dirname + "../../dist"));
+
+app.listen(NODE_PORT, function () {
+    console.info("App server started on port " + NODE_PORT);
+});
