edf458d5d78b697bb71e7e792ed028fc1c2f3b05
Douglas Sim <douglasswm@gmail.com>
2018-05-09 13:25:11 +0800

    fix model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,10 @@ import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HomeModule } from './home/home.module';
-import { MarketDataModule } from './market-data/market-data.module';
 import {
-  FooterComponent,
-  HeaderComponent,
-  SharedModule
+    FooterComponent,
+    HeaderComponent,
+    SharedModule
 } from './shared';
 import { AppRoutingModule } from "./app-routing.module";
 import { CoreModule } from './core/core.module';
--- src/app/market-data/market-data.component.ts
+++ src/app/market-data/market-data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
-import { CryptoInfoService, CryptoInfo } from '../core';
+import { CryptoInfoService, CryptoInfo, A } from '../core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs/Observable';
 import { ifError } from "assert";
@@ -9,67 +9,88 @@ import { ifError } from "assert";
     styleUrls: ['./market-data.component.scss'],
     templateUrl: './market-data.component.html',
 })
-export class MarketDataComponent {
+export class MarketDataComponent implements OnInit {
 
-  fiveData : Array<any>
-  cryptoInfo =[]as Array<CryptoInfo>
-  cryptoConvertInfo: CryptoInfo
-  crypto : CryptoInfo
+    fiveData: Array<any>
+    cryptoInfo = [] as Array<CryptoInfo>
+    cryptoConvertInfo: CryptoInfo
+    crypto: CryptoInfo
+    private a = new A(
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    )
 
-  constructor(
-        // private cryptoInfoService : CryptoInfoService,
-        private http: HttpClient) {
 
-        // this.getMarketData().subscribe(result => {
-        //     console.log(result.data)
-        //     this.cryptoInfo = result.data
-        //     console.log(this.cryptoInfo)
-        //     console.log(this.cryptoInfo.length)
-        //     console.log(typeof this.cryptoInfo)
-        //     for (let i=0; i<this.cryptoInfo.length; i++) {
-        //       // copy.push(items[i])
-        //       if (i==51||i==327||i==511||i==1026||i==2009) {
-        //         this.fiveData.push(this.cryptoInfo[i])
-        //         console.log(this.fiveData.length)
-        //       } else {
-        //         //
-        //       }
-        //      }
-        // })
-        // this.getMarketConvertData(2, 'BCH').subscribe(result => {
-        //     console.log(result)
-        //     this.cryptoConvertInfo = result.data
-        //     console.log(this.cryptoConvertInfo)
-        // })
+    constructor(private http: HttpClient) {
 
+    }
+
+    ngOnInit() {
         this.getAllFiveData(52).subscribe(result => {
-          this.crypto = result.data
+            this.a = result.data
         })
+    }
 
-        
-        // this.cryptoInfo.forEach((crypto,index) => {
-        //   if (index==52||index==328||index==512||index==1027||index==2010) {
-        //     this.fiveData.push(crypto)
-        //     console.log(this.fiveData.length)
-        //   } else {
-        //     //
-        //   }
-        // })
-      }
 
-    // getMarketData(): Observable<any> {
-    //     let url = `https://api.coinmarketcap.com/v2/listings/`
-    //     // let url = `https://api.coinmarketcap.com/v2/ticker/${id}/`
-    //     return this.http.get(url);
-    // }
+    getAllFiveData(id): Observable<any> {
+        let url = `https://api.coinmarketcap.com/v2/ticker/${id}/`
+        return this.http.get(url);
+    }
 
-    getAllFiveData(id):Observable<any> {
-      let url = `https://api.coinmarketcap.com/v2/ticker/${id}/`
-      return this.http.get(url);
-  }
-    // getMarketConvertData(id, convert) {
-    //     let url = `https://api.coinmarketcap.com/v2/ticker/${id}/?convert=${convert}`
-    //     return this.http.get<{ data: any, metadata: any }>(url);
-    // }
 
 }
+
+// getMarketConvertData(id, convert) {
+//     let url = `https://api.coinmarketcap.com/v2/ticker/${id}/?convert=${convert}`
+//     return this.http.get<{ data: any, metadata: any }>(url);
+// }
+
+
+
+// this.getMarketData().subscribe(result => {
+//     console.log(result.data)
+//     this.cryptoInfo = result.data
+//     console.log(this.cryptoInfo)
+//     console.log(this.cryptoInfo.length)
+//     console.log(typeof this.cryptoInfo)
+//     for (let i=0; i<this.cryptoInfo.length; i++) {
+//       // copy.push(items[i])
+//       if (i==51||i==327||i==511||i==1026||i==2009) {
+//         this.fiveData.push(this.cryptoInfo[i])
+//         console.log(this.fiveData.length)
+//       } else {
+//         //
+//       }
+//      }
+// })
+// this.getMarketConvertData(2, 'BCH').subscribe(result => {
+//     console.log(result)
+//     this.cryptoConvertInfo = result.data
+//     console.log(this.cryptoConvertInfo)
+// })
+
+
+
+// this.cryptoInfo.forEach((crypto,index) => {
+//   if (index==52||index==328||index==512||index==1027||index==2010) {
+//     this.fiveData.push(crypto)
+//     console.log(this.fiveData.length)
+//   } else {
+//     //
+//   }
+// })
+
+
+// getMarketData(): Observable<any> {
+//     let url = `https://api.coinmarketcap.com/v2/listings/`
+//     // let url = `https://api.coinmarketcap.com/v2/ticker/${id}/`
+//     return this.http.get(url);
+// }
