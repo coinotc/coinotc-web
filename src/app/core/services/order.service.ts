8662c1cf50fc764f3ed017d95f800db5d51c1b05
louishwy <905615218@qq.com>
2018-06-14 18:50:39 +0800

    feat order details

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -8,31 +8,34 @@ import { map } from 'rxjs/operators/map';
 
 @Injectable()
 export class OrderService {
-  constructor(private apiService: ApiService) { }
+    constructor(private apiService: ApiService) {}
 
-  public getOrders(username, finished) {
-    let getURL = `/order/filter?username=${username}&finished=${finished}`;
-    return this.apiService.get(getURL);
-  }
+    public getOrders(username, finished) {
+        let getURL = `/order/filter?username=${username}&finished=${finished}`;
+        return this.apiService.get(getURL);
+    }
 
-  public createOrder(order) {
-    let url = `/order`
-    return this.apiService.post(url, order)
-  }
+    public createOrder(order) {
+        return this.apiService.post('/order', order);
+    }
 
-  public getByID(_id) {
-    let url = `/order/getone?_id=${_id}`
-    return this.apiService.get(url, _id)
-  }
+    public updateOrder(order) {
+        return this.apiService.put('/order', order);
+    }
 
-  public addRoomKey(roomkey, orderId) {
-    let URL = `/order/roomkey?orderId=${orderId}`;
-    return this.apiService.patch(URL, { roomkey: roomkey });
-  }
+    public getByID(_id) {
+        let url = `/order/getone?_id=${_id}`;
+        return this.apiService.get(url, _id);
+    }
 
-  //according to owner to get orders
-  getOrderWithHim(profileUser, currentUser): Observable<OrderInformation[]> {
-    let url = `/order/tradeWithHim?profileUser=${profileUser}&currentUser=${currentUser}`;
-    return this.apiService.get(url);
-  }
+    public addRoomKey(roomkey, orderId) {
+        let URL = `/order/roomkey?orderId=${orderId}`;
+        return this.apiService.patch(URL, { roomkey: roomkey });
+    }
+
+    //according to owner to get orders
+    getOrderWithHim(profileUser, currentUser): Observable<OrderInformation[]> {
+        let url = `/order/tradeWithHim?profileUser=${profileUser}&currentUser=${currentUser}`;
+        return this.apiService.get(url);
+    }
 }
