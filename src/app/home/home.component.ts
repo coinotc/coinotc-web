bf5fb49e38320fb5abd79adba16ae156d2adff3f
Mr-Mooo <hjchoujiacheng@126.com>
2018-06-14 17:54:31 +0800

    change logout button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -39,11 +39,6 @@ export class HomeComponent implements OnInit {
   onEdit(advert){
     this.advDetailService.detailAdv(advert)
   }
-   
-  logout() {
-    this.userService.purgeAuth();
-    this.router.navigateByUrl('/');
-  }
   ngOnInit() {
     this.refreshData();
   }
