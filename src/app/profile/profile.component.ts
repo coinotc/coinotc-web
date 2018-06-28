38c4f3d3eb17846a6600f6d7cf092f57619fe484
Mr-Mooo <hjchoujiacheng@126.com>
2018-06-28 15:02:29 +0800

    add basic settings in profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as _ from 'lodash';
 import {
     User,
     UserService,
@@ -10,7 +11,8 @@ import {
     OrderService,
     advertisement,
     AdvDetailService,
-    ProfilesService
+    ProfilesService,
+    CurrenciesService
 } from '../core';
 import { concatMap } from 'rxjs/operators/concatMap';
 import { tap } from 'rxjs/operators/tap';
@@ -20,6 +22,16 @@ import { tap } from 'rxjs/operators/tap';
     templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
+    currencies: any[];
+    regions = [
+        { label: 'Global', value: 'global' },
+        { label: 'Singapore', value: 'singapore' },
+        { label: 'China', value: 'china' },
+        { label: 'Malaysia', value: 'malaysia' },
+        { label: 'Korea', value: 'korea' },
+        { label: 'Thailand', value: 'thailand' }
+    ];
+    languages = [{ label: 'English', value: 'en' }, { label: '中文', value: 'cn' }];
     profile: Profile;
     currentUser: User;
     isUser: boolean;
@@ -36,15 +48,33 @@ export class ProfileComponent implements OnInit {
         private userService: UserService,
         private advServise: AdvertisementsService,
         private orderService: OrderService,
-        private advDetailService:AdvDetailService,
-        private profilesService:ProfilesService
+        private advDetailService: AdvDetailService,
+        private profilesService: ProfilesService,
+        private currencyService: CurrenciesService,
     ) {
+        this.initializeCurrencies();
         this.userService.getUser().subscribe(result => {
             this.user = result;
             console.log(this.user)
         })
     }
+    initializeCurrencies() {
+        this.currencyService.getCurrencies().subscribe(currencies => {
+            console.log("currencies ==> " + currencies);
+            let currenciesCode = _.keys(currencies);
+            let currenciesDesc = _.values(currencies);
+            let currenciesArr = [];
 
+            for (let currencyIndex in currenciesCode) {
+                let currencyObj = {
+                    currenciesCode: currenciesCode[currencyIndex],
+                    currenciesDesc: currenciesDesc[currencyIndex]
+                }
+                currenciesArr.push(currencyObj);
+            }
+            this.currencies = currenciesArr;
+        });
+    }
     onStatus(profileStatus) {
         this.status = profileStatus;
         this.router.navigateByUrl(`/profile/${this.user.username}`);
@@ -88,39 +118,64 @@ export class ProfileComponent implements OnInit {
                 console.log(this.adverts);
             });
     }
-    getfollow(profileStatus, type){
+    getfollow(profileStatus, type) {
         let follow = []
-        if(type == true){
-            follow = this.currentUser.following;
-        }else{
-            follow = this.currentUser.followers;
+        if (type == true) {
+            follow = this.userService.getCurrentUser().following;
+        } else {
+            follow = this.userService.getCurrentUser().followers;
         }
         console.log(follow)
-        this.profilesService.getProfileInfo(follow).subscribe(result=>{
-          console.log(result);
-          this.profiles = result;
-        this.status = profileStatus;
-        this.router.navigateByUrl(`/profile/${this.user.username}`);  
+        this.profilesService.getProfileInfo(follow).subscribe(result => {
+            console.log(result);
+            this.profiles = result;
+            this.status = profileStatus;
+            this.router.navigateByUrl(`/profile/${this.user.username}`);
+        })
+    }
+    unfollow(follow) {
+        console.log(follow)
+        this.profilesService.getProfile(follow.username).subscribe(result => {
+            let a = result[0].followers;
+            let b = this.userService.getCurrentUser().following;
+            console.log(a)
+            console.log(b)
+            b.splice(
+                this.userService.getCurrentUser().following.indexOf(follow.username),
+                1
+            );
+            a.splice(
+                this.userService
+                    .getCurrentUser()
+                    .following.indexOf(this.currentUser.username),
+                1
+            );
+            console.log(a)
+            console.log(b)
+
+            this.profilesService.sendFollowing(this.currentUser.username, b).subscribe();
+            this.profilesService.sendFollowers(follow.username, a).subscribe();
+            this.getfollow("following", true)
         })
     }
     setVisible(information) {
         this.advServise
-          .changeVisible(information._id, information.visible)
-          .subscribe(() => {
-            this.advServise
-            .getByOwner(this.currentUser.username, information.visible)
-            .subscribe(result => {
-                this.adverts = result;
-                console.log(this.adverts);
+            .changeVisible(information._id, information.visible)
+            .subscribe(() => {
+                this.advServise
+                    .getByOwner(this.currentUser.username, information.visible)
+                    .subscribe(result => {
+                        this.adverts = result;
+                        console.log(this.adverts);
+                    });
             });
-          });
-      }
-      editAdvertisement(information){
+    }
+    editAdvertisement(information) {
         this.status = "advertsEdit";
         this.advertisementInfo = information;
         //this.advDetailService.detailAdv(information);
         this.router.navigateByUrl(`/profile/${this.user.username}`);
-      }
+    }
     // onToggleFollowing(following: boolean) {
     //   this.profile.following = following;
     // }
